1951ba684c1fbf9517a1476b0742e5a7491ed13d
Anantha Raju C <anantha@platformcommons.com>
2022-04-26 07:46:42 +0530

    Initial Commit

---
+++ src/main/java/com/yawintutor/SpringBootBatchDbToDbApplication.java
@@ -0,0 +1,13 @@
+package com.yawintutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchDbToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchDbToDbApplication.class, args);
+	}
+
+}
