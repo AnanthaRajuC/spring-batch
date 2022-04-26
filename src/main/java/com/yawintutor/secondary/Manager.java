e48e38fedbd5f9341217ee14be9f04065dbc1efe
Anantha Raju C <anantha@platformcommons.com>
2022-04-26 07:54:09 +0530

    ADDED: Lombok Anotations

--- src/main/java/com/yawintutor/secondary/Manager.java
+++ src/main/java/com/yawintutor/secondary/Manager.java
@@ -1,33 +1,18 @@
 package com.yawintutor.secondary;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "manager")
+@Getter
+@Setter
 public class Manager {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private int id;
     private String name;
     private int salary;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getSalary() {
-        return salary;
-    }
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
 }
