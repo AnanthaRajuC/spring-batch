457a211fe2f702dc76964bbd8d3068a0e159b496
Anantha Raju C <anantha@platformcommons.com>
2022-04-26 09:03:44 +0530

    REFACTORED: Main Class Name

--- src/main/java/org/platformcommons/PtldToAssessmentDbDataCopyApplication.java
+++ src/main/java/org/platformcommons/PtldToAssessmentDbDataCopyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBatchDbToDbApplication {
+public class PtldToAssessmentDbDataCopyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBatchDbToDbApplication.class, args);
+		SpringApplication.run(PtldToAssessmentDbDataCopyApplication.class, args);
 	}
 }
